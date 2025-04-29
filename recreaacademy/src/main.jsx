7b1dfca1747dbf0111a7d95a7778435627b4931e
aleyrasg <126541501+aleyrasg@users.noreply.github.com>
2025-04-29 11:05:00 -0600

    rutas

--- recreaacademy/src/main.jsx
+++ recreaacademy/src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Inicio/Home';
+import RutasFormativas from './pages/RutasFormativas/RutasFormativas';
+import Innovaciones from './pages/Innovaciones/Innovaciones';
+import Colaboraciones from './pages/Colaboraciones/Colaboraciones';
+import Eventos from './pages/Eventos/Eventos';
+import MiPortafolio from './pages/Portafolio/MiPortafolio';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  // {
+    
+  //   path: '/',
+  //   element: <Home />,
+  // },
+  {
+    path: '/rutas-formativas',
+    element: <RutasFormativas />,
+  },
+  {
+    path: '/innovaciones',
+    element: <Innovaciones />,
+  },
+  {
+    path: '/colaboraciones',
+    element: <Colaboraciones />,
+  },
+  {
+    path: '/eventos',
+    element: <Eventos />,
+  },
+  {
+    path: '/mi-portafolio',
+    element: <MiPortafolio />,
+  },
+
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
