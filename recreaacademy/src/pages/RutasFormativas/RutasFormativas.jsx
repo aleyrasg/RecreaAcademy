fe44fe6e9b5f6b5bcb107d378b347a2b57b8ba10
aleyrasg <126541501+aleyrasg@users.noreply.github.com>
2025-05-02 13:57:18 -0600

    Rutas Formativas .css

--- recreaacademy/src/pages/RutasFormativas/RutasFormativas.jsx
+++ recreaacademy/src/pages/RutasFormativas/RutasFormativas.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React from 'react';
+import Layout from '../../components/Layout/Layout';
+import './RutasFormativas.css';
+import {
+  FaBook,
+  FaGraduationCap,
+  FaFire,
+  FaTrophy,
+  FaTools,
+  FaHeart,
+  FaVial,
+  FaUserGraduate,
+} from 'react-icons/fa';
 
 function RutasFormativas() {
   return (
     <Layout>
       <h1>Rutas Formativas</h1>
-      {/* Aquí iría el contenido adicional */}
+      <p>¡Conoce cada una de nuestras rutas formativas!</p>
+
+      <div className="rotating-circle-container">
+        <div className="rotating-circle">
+          <div className="icon"><FaBook /></div>
+          <div className="icon"><FaGraduationCap /></div>
+          <div className="icon"><FaFire /></div>
+          <div className="icon"><FaTrophy /></div>
+          <div className="icon"><FaTools /></div>
+          <div className="icon"><FaHeart /></div>
+          <div className="icon"><FaVial /></div>
+          <div className="icon"><FaUserGraduate /></div>
+        </div>
+
+        <div className="center-circle">
+          <div className="center-text">
+            <h2>Acércate para conocer</h2>
+            <p>¿Qué son las Rutas Formativas?</p>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
+  );
 }
 
-export default RutasFormativas
+export default RutasFormativas;
